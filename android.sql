acf42f9b15e44138a04cb09d11f4cd0640d6b8cc
dangerousor <1192350292@qq.com>
2018-11-25 15:36:54 +0800

    video add topic

---
+++ android.sql
@@ -0,0 +1,27 @@
+﻿SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS  `video`;
+CREATE TABLE `video` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) DEFAULT NULL,
+  `video` varchar(256) DEFAULT NULL,
+  `title` text,
+  'topic' varchar(128),
+  `location` text,
+  `time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS  `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nickname` varchar(32) DEFAULT NULL,
+  `username` varchar(32) DEFAULT NULL,
+  `password` varchar(128) DEFAULT NULL,
+  `bitmap` longtext,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_username_uindex` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
